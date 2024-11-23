0b0a869319e2e35b0eec02c64becf92b3b841840
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-23 12:29:31 -0500

    Update scripture display for responsiveness

--- src/components/Scripture.tsx
+++ src/components/Scripture.tsx
@@ -21,12 +21,14 @@ const Scripture: React.FC<emotionsProps> = ({ inputScreen, setInputScreen, rando
 
     return(
         <>
+        <Box visibleFrom="xs">
         { randomScripture ? (
             <Box mb="xl" pb="md" pt="md" mt="xl"> 
                 <Title order={3}>"{randomScripture.verse}"</Title>
                 <Flex mt="lg" direction="row" align="center" justify="center" gap={6}>
                 <Text  size="xl">({randomScripture.location}</Text> 
-                <Tooltip position="bottom-start" offset={-3} label="Translation">
+                <Tooltip withArrow position="bottom-start" offset={-3} label="Translation"
+                 transitionProps={{ transition: 'scale-x', duration: 300 }}>
                 <Text c="blue" size="xl">{randomScripture.translation}<Text size="xl" c="#000" span>)</Text></Text>
                 </Tooltip>
                 </Flex>
@@ -34,10 +36,30 @@ const Scripture: React.FC<emotionsProps> = ({ inputScreen, setInputScreen, rando
         ) : (
             <p>Please select emotions and try again.</p>
         )}
-        <Flex gap="md" >
+        <Flex justify="center" gap="md" >
         <Button size="md" onClick={backToHome} style={{alignItems: "center"}}  rightSection={<IconMoodPlus  style={{marginLeft: "-0.2rem" }}  stroke={2.2} size={20} />} variant="light">I'm feeling different</Button>
         <Button size="md" onClick={scripturePicker} >Give Me Another <Text mt={1} ml={6} span>✨</Text></Button>
         </Flex>
+        </Box>
+
+        <Box hiddenFrom="xs">
+        { randomScripture ? (
+            <Box mb="lg" pb="sm" pt="md" mt="xl"> 
+                <Title order={4}>"{randomScripture.verse}"</Title>
+                <Flex mt="lg" direction="row" align="center" justify="center" gap={6}>
+                <Text  size="md">({randomScripture.location}</Text> 
+                <Text c="blue" size="md">{randomScripture.translation}<Text size="md" c="#000" span>)</Text></Text>
+                </Flex>
+            </Box>
+        ) : (
+            <p>Please select emotions and try again.</p>
+        )}
+        <Flex justify="center" gap="md" >
+        <Button size="sm" onClick={backToHome} style={{alignItems: "center"}}  rightSection={<IconMoodPlus  style={{marginLeft: "-0.2rem" }}  stroke={2.2} size={20} />} variant="light">I'm feeling different</Button>
+        <Button size="sm" onClick={scripturePicker} >Give Me Another <Text mt={1} ml={6} span>✨</Text></Button>
+        </Flex>
+        </Box>
+
         </>
     )
 }
