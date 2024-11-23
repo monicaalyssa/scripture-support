c833a866ade2772a8681517db3c528d10c8c0fa9
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-23 14:53:30 -0500

    Add PWA capabilities and service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.png', 'favicon180.png'],
+      manifest: {
+        name: "Scripture Support",
+        short_name: "SS",
+        description: "Find Comfort and Encouragement in God's Word",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/favicon192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/favicon512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
